06404fbc1c0c386e54892d4501843cbcb6ba2c99
Luis Díaz <ldiazn98@gmail.com>
2025-09-04 12:25:32 +0200

    Revert "Test fixed-sized version of echo test helper against client"
    This reverts commit 6b11203e17fcd12c5b356d320f0bdc922e0bd962.
    
    I tested this version against the miniooni client and returns the same
    failure, so it's not a problem with the previous implementation

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,33 +1,30 @@
-use log::{debug, error, info};
-use test_helpers::helper_runner::run_tcp_server;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpStream,
-};
+use log::{error, info};
+use test_helpers::helper_runner::{read_port, run_tcp_server};
+use tokio::{io, net::TcpStream};
 
 #[tokio::main]
 async fn main() {
-    run_tcp_server("echoth", "8000", handle_tcp_echo).await;
+    let port = read_port("8000");
+    run_tcp_server("echoth", &port, handle_tcp_echo).await;
 }
 
 async fn handle_tcp_echo(mut stream: TcpStream) {
-    let mut buffer = [0u8; 4096];
+    // For development, an easy way to test this function is starting the server and using telnet
+    // to send some data and check if the data comes back properly
+    //
+    // ```
+    // telnet localhost 8000
+    // ```
     info!("Connection received");
-    loop {
-        match stream.read(&mut buffer).await {
-            Ok(0) => {
-                info!("Connection closed");
-                break;
-            }
-            Ok(n) => {
-                debug!("Receiving {n} bytes");
-                if let Err(e) = stream.write_all(&buffer[0..n]).await {
-                    error!("Error trying to write response {e}");
-                }
-            }
-            Err(e) => {
-                error!("Error trying to read from request: {e}");
-            }
-        };
+
+    let (mut reader, mut writer) = stream.split();
+
+    // Note that this function will get stucked here until the client closes the connection,
+    // continuosly sending the data it receives. This is expected.
+    let result = io::copy(&mut reader, &mut writer).await;
+    match result {
+        Ok(0) => info!("Connection closed"),
+        Ok(n) => info!("Received {n} bytes in total"),
+        Err(e) => error!("Error processing request: {e}"),
     }
 }
